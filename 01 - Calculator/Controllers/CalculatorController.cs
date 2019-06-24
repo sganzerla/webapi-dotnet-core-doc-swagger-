ee0ca98235f274f0a5c5b9fb65e3f4bacef97173
sganzerla <alisson_sganzerla@yahoo.com.br>
2019-06-23 23:51:29 -0300

    Add Operations Calculator

--- 01 - Calculator/Controllers/CalculatorController.cs
+++ 01 - Calculator/Controllers/CalculatorController.cs
@@ -11,15 +11,66 @@ namespace webapi_dotnet_core_doc_swagger_.Controllers
     public class CalculatorController : ControllerBase
     {
         
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public ActionResult<string> Sum(string firstNumber,string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber)){
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
+                var result = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
             }
             return BadRequest("Invalid value");
         }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Subtraction(string firstNumber,string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber)){
+                var result = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+         [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Division(string firstNumber,string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber)){
+                var result = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Multiplication(string firstNumber,string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber)){
+                var result = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mean(string firstNumber,string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber)){
+                var result = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
+        
+        [HttpGet("square-root/{number}")]
+        public ActionResult<string> SquareRoot(string number )
+        {
+            if(IsNumeric(number) ){
+                var result = Math.Sqrt((double)ConvertToDecimal(number)) ;
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid value");
+        }
+
         private decimal ConvertToDecimal(string number) {
             decimal decimalValue;
             if (decimal.TryParse(number, out decimalValue)){
@@ -34,6 +85,7 @@ namespace webapi_dotnet_core_doc_swagger_.Controllers
             bool isNumber = double.TryParse(number, 
             System.Globalization.NumberStyles.Any, 
             System.Globalization.NumberFormatInfo.InvariantInfo , out numberConverted);
+            //GLobalization mantém formato númerico
             return isNumber;
         }
     }
